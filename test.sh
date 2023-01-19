9da2c1c9ee6b8cdb18e378fd0ebefd0cb44d764e
a-sotnikov <sotnikov.andrey07@gmail.com>
2023-01-19 09:42:21 +0300

    dev, some improvments

--- test.sh
+++ test.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
-echo "I have no idea about $1"
+if [ $# -ge 1 ]; then
+    echo "I have no idea about $@"
+fi
 
 #Print some system info
 echo "Current date: $(date)"
 echo "You are $USER"
 echo "Current dir: $PWD"
-
+echo
 cowsay git is a great tool! 2>/dev/null
 if [[ $? -ne 0 ]]; then
+    echo
+    sleep 2
+    echo "Wait a minute..."
+    sleep 2
     echo "Why you havn't install cowsay?"
+    sleep 2
     echo "Do you have something against cows, do you??"
 fi
