932374f34b863f9b79252fc742faa1679563bb1f
a-sotnikov <sotnikov.andrey07@gmail.com>
2023-01-19 09:31:41 +0300

    dev, added cool feature

--- test.sh
+++ test.sh
@@ -6,3 +6,9 @@ echo "I have no idea about $1"
 echo "Current date: $(date)"
 echo "You are $USER"
 echo "Current dir: $PWD"
+
+cowsay git is a great tool! 2>/dev/null
+if [[ $? -ne 0 ]]; then
+    echo "Why you havn't install cowsay?"
+    echo "Do you have something against cows, do you??"
+fi
